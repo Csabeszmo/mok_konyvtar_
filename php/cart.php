54b0c224bedcb6e5e577a7cc5c61211040a09155
JerneiAkos05 <jernei.akos-2020@keri.mako>
2025-01-29 13:25:01 +0100

    kosár és profil php-jának létrehozása

--- php/cart.php
+++ php/cart.php
@@ -4,3 +4,22 @@ declare(strict_types=1);
 
 require_once("./environment.php");
 
+$db = new Database();
+
+$query = "SELECT `books`.`title`, 
+                 `cart`.`loan_start_date`, 
+                 `cart`.`return_date`, 
+                 `cart`.`return_status`, 
+                 `cart_items_books`.`db` 
+          FROM `cart` 
+          INNER JOIN `cart_items_books` 
+          ON `cart`.`cart_id` = `cart_items_books`.`cart_id` 
+          INNER JOIN `books` 
+          ON `cart`.`book_id` = `books`.`book_id` 
+          WHERE `cart`.`return_status` = 1";
+
+$result = $db->execute($query);
+
+$db = null;
+
+echo json_encode($result);
