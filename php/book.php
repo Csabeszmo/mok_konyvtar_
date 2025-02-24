7dbcd80860331d9b32d1fd432f2a53e2b8c39e24
JerneiAkos05 <jernei.akos-2020@keri.mako.hu>
2025-02-24 19:13:16 +0100

    author tábla hozzáadása a books táblához

--- php/book.php
+++ php/book.php
@@ -3,11 +3,15 @@ declare(strict_types=1);
 
 require_once("../../common/php/environment.php");
 
-$args = Util::getArgs();
+$db = new Database();
 
 $query = "SELECT `books`.`title`, 
                  `authors`.`first_name`,
                  `authors`.`last_name`,
+                 `authors`.`biography`,
+                 `authors`.`author_image_url`,
+                 `authors`.`birth_date`,
+                 `authors`.`death_date`,
                  `books`.`translator`,
                  `books`.`publisher`,
                  `books`.`series`,
@@ -19,17 +23,16 @@ $query = "SELECT `books`.`title`,
                  `books`.`detailed_description`,
                  `books`.`cover_image_url`,
                  `categories`.`categories_name`
-          FROM `books`
-          INNER JOIN `authors`
-          ON `books`.`author_id` = `authors`.`author_id`
-          INNER JOIN `categories`
-          ON `books`.`categories_id` = `categories`.`categories_id`
-          WHERE `books`.`book_id` = :book_id
-          LIMIT 1;";
+            FROM `books`
+      INNER JOIN `authors`
+              ON `books`.`author_id` = `authors`.`author_id`
+      INNER JOIN `categories`
+              ON `books`.`categories_id` = `categories`.`categories_id`
+           WHERE `books`.`book_id` = :book_id
+           LIMIT 1;";
 
-$db = new Database();
 
-$result = $db->execute($query, $args);
+$result = $db->execute($query);
 
 $db = null;
 
