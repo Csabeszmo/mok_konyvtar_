cc5426d87f7beccf2f2e89fd4841cae7332c87d6
JerneiAkos05 <jernei.akos-2020@keri.mako.hu>
2025-03-18 11:22:33 +0100

    eseményekre regisztráció

--- php/addBook.php
+++ php/addBook.php
@@ -10,21 +10,20 @@ $query = "INSERT INTO `cart`
                      (`user_id`) 
               VALUES (:user_id)";
 
-$result1 = $db->execute($query, $args);
+$result['cart'] = $db->execute($query, $args);
 
 $query = "INSERT INTO `cart_items_books`
                      (`cart_id`, 
-                      `book_id`, 
-                      `db`) 
+                      `book_id`) 
              VALUES  (:cart_id, :book_id)";
 
-$result2 = $db->execute($query, $args);
+$result['cart_items_books'] = $db->execute($query, $args);
 
 $query = "UPDATE `cart_items_books` SET 
-                 `db`= `db`+1
+                 `db`=`db`+1
            WHERE `user_id` = :user_id;";
 
-$result3 = $db->execute($query, $args);
+$result = $db->execute($query, $args);
 
 $db = null;
 
