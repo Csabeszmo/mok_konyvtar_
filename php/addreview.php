421d1c9644b550c56d098671440d2fb261993b49
Csabeszmo <selmeczi.csaba-2020@keri.mako.hu>
2025-04-08 11:11:07 +0200

    addreviewphp

--- php/addreview.php
+++ php/addreview.php
@@ -1,11 +1,24 @@
 <?php
+
 declare(strict_types=1);
 
-require_once('./environment.php');
+require_once('../../common/php/environment.php');
 
 $args = Util::getArgs();
 
 $db = new Database();
+
+$query= "SELECT `user_id` 
+           FROM `users` 
+          WHERE `email` = ?
+          LIMIT 1;";
+
+$result = $db->execute($query, $args['email']);
+
+if (!is_null($result)) {
+    $db = null;
+    Util::setError('A felhasználó ezen az email címen már létezik!');
+}
 
 $query= "INSERT INTO `reviews` 
                     (`user_id`,
@@ -18,9 +31,12 @@ $query= "INSERT INTO `reviews`
                  :rating,
                  :review_text,
                  :review_date)";
-
-$result = $db->execute($query, $args);
+$result = $db->execute($query, array_values($args));
 
 $db = null;
+
+if(!$result['affectedRows']){
+    Util::setError("Nem működik a regisztráció!");
+}
 
 Util::setResponse($result);
